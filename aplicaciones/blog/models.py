0b135769e3acfa0350645131960dc80361d32250
danielagoston <danielagostonschrotlin@gmail.com>
2024-02-22 16:08:22 -0300

    Personalizar vista de modelos en admin de django

--- aplicaciones/blog/models.py
+++ aplicaciones/blog/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Categoria(models.Model):
     id = models.AutoField(primary_key = True)
-    nombre = models.CharField(max_length = 100, null = False, blank = False)
-    estado = models.BooleanField(default = True)
-    fecha_creacion = models.DateField(auto_now = False, auto_now_add = True)
+    nombre = models.CharField('Nombre de la Categoría', max_length = 100, null = False, blank = False)
+    estado = models.BooleanField('Categoría Activada/Categoría no Activada', default = True)
+    fecha_creacion = models.DateField('Fecha de Creación', auto_now = False, auto_now_add = True)
 
     class Meta:
         verbose_name = 'Categoría'
@@ -15,15 +15,15 @@ class Categoria(models.Model):
     
 class Autor(models.Model):
     id = models.AutoField(primary_key = True)
-    nombres = models.CharField(max_length = 255, null = False, blank = False)
-    apellidos = models.CharField(max_length = 255, null = False, blank = False)
-    facebook = models.URLField(null = True, blank = True)
-    instagram = models.URLField(null = True, blank = True)
-    twitter = models.URLField(null = True, blank = True)
-    web = models.URLField(null = True, blank = True)
-    correo = models.EmailField(null = False, blank = False)
-    estado = models.BooleanField(default = True)
-    fecha_creacion = models.DateField(auto_now = False, auto_now_add = True)
+    nombres = models.CharField('Nombres del Autor', max_length = 255, null = False, blank = False)
+    apellidos = models.CharField('Apellidos del Autor', max_length = 255, null = False, blank = False)
+    facebook = models.URLField('Facebook', null = True, blank = True)
+    instagram = models.URLField('Instagram', null = True, blank = True)
+    twitter = models.URLField('Twitter', null = True, blank = True)
+    web = models.URLField('Web', null = True, blank = True)
+    correo = models.EmailField('Correo Electrónico', null = False, blank = False)
+    estado = models.BooleanField('Autor Activo/ No Activo', default = True)
+    fecha_creacion = models.DateField('Fecha de Creación', auto_now = False, auto_now_add = True)
 
     class Meta:
         verbose_name = 'Autor'
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -76,12 +76,17 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blog',
-        'USER':  'postgres',
-        'PASSWORD':  'paloma227',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    
+    
+    #'default': {
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'blog',
+        #'USER':  'postgres',
+        #'PASSWORD':  'paloma227',
+        #'HOST': 'localhost',
+        #'PORT': '5432'
     }
 }
 
