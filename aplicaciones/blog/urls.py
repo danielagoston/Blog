917d537862b2e6cbd79c6d11d4a269d714b4a226
danielagoston <danielagostonschrotlin@gmail.com>
2024-02-27 17:43:21 -0300

    Agregar listado y visualizar post

--- aplicaciones/blog/urls.py
+++ aplicaciones/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home, about, anecdotes, purpose
+from .views import home, about, anecdotes, purpose,detallePost
 
 urlpatterns = [
     path('',home, name = 'index'),
-    path('about/',about, name = 'about'),
-    path('anecdotes/',anecdotes, name = 'anecdotes'),
-    path('purpose/',purpose, name = 'purpose'),
+    path('<slug:slug>/', detallePost, name = 'detalle_post'),
+    path('about/', about, name = 'about'),
+    path('anecdotes/', anecdotes, name = 'anecdotes'),
+    path('purpose/', purpose, name = 'purpose'),
 
 ]
--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
+from .models import Post, Categoria
 
 def home(request):
-    return render (request, 'index.html')
+    posts = Post.objects.filter(estado = True)
+    return render (request, 'index.html', {'posts':posts})
 
+def detallePost(request,slug):
+    post = Post.objects.get(
+        slug = slug
+    )
+    return render(request,'post.html', {'detalle_post':post})
 def about(request):
-    return render (request, 'about.html')
+    posts = Post.objects.filter(
+        estado = True, 
+        categoria = Categoria.objects.get(nombre = '¿Quiénes Somos?'))
+    return render (request, 'about.html', {'posts':posts})
 
 def anecdotes(request):
-    return render (request, 'anecdotes.html')
+    posts = Post.objects.filter(
+        estado = True, 
+        categoria = Categoria.objects.get(nombre = 'Anécdotas'))
+    return render (request, 'anecdotes.html', {'posts':posts})
 
 def purpose(request):
-    return render (request, 'purpose.html')
+    posts = Post.objects.filter(
+        estado = True, 
+        categoria = Categoria.objects.get(nombre = 'Propósito'))
+    return render (request, 'purpose.html', {'posts':posts})
